5eeea791506ff389ecdd396990c28e783013866b
nmartinez3 <n_martinez3@u.pacific.edu>
2018-10-03 22:28:16 -0700

    adding more SQL scripts

--- create and insert practice.sql
+++ create and insert practice.sql
@@ -1,3 +1,5 @@
+USE sakila;
+
 CREATE TABLE test_table(
 	film_id int auto_increment NOT NULL,
     col2 varchar(64),
@@ -17,3 +19,11 @@ INSERT INTO test_table(film_id, col2)
 SELECT * FROM test_table;
 
 SHOW INDEXES FROM test_table;
+
+INSERT INTO test_table(film_id, col2)
+	SELECT film_id + 1, col2 FROM test_table ORDER BY rand();
+
+# well, trying to change row order has no effect on how the table is displayed when calling SELECT *;
+UPDATE test_table SET film_id = film_id WHERE film_id >= 1 ORDER BY film_id DESC;
+
+SELECT * FROM test_table;
---
+++ query optimization practice 1.sql
@@ -0,0 +1,13 @@
+USE sakila;
+
+DESCRIBE film;
+
+SHOW KEYS FROM film;
+
+SELECT film_id, release_year FROM film WHERE rental_rate <= 2.99 ORDER BY rating;
+
+SELECT DISTINCT(rating) FROM film ORDER BY rating;
+
+CREATE INDEX rating_idx ON film (rating);
+
+SELECT COUNT(*)FROM film WHERE rating IS NULL;
